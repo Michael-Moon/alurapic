04e7f17138364bda8cd2f52e51a4f633e1c045c4
Michael <44618004+Michael-Moon@users.noreply.github.com>
2022-01-22 14:29:18 -0300

    ajuste component photo form

--- src/app/photos/photo-form/photo-form.component.ts
+++ src/app/photos/photo-form/photo-form.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+import { Router } from '@angular/router';
+import { PhotoService } from './../photo/photo.service';
+
 @Component({
   selector: 'ap-photo-form',
   templateUrl: './photo-form.component.html',
@@ -7,9 +13,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PhotoFormComponent implements OnInit {
 
-  constructor() { }
+  photoForm: FormGroup;
+  file: File;
+  preview: string;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private photoService: PhotoService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+    this.photoForm = this.formBuilder.group({
+      file: ['', Validators.required],
+      description: ['', Validators.maxLength(300)],
+      allowComments: [true]
+    })
+  }
+
+
+  upload(){
+    const description = this.photoForm.getRawValue().description;
+    const allowComments = this.photoForm.getRawValue().allowComments;
+
+    this.photoService.upload(description, allowComments, this.file)
+    .subscribe( ()=> this.router.navigate(['']))
+  }
+
+  handleFile(file: File){
+    this.file = file;
+    const reader = new FileReader()
+    reader.onload = (event: any) => this.preview = event.target.result;
+    reader.readAsDataURL(file);
   }
 
 }
--- src/app/photos/photo-form/photo-form.module.ts
+++ src/app/photos/photo-form/photo-form.module.ts
@@ -1,9 +1,24 @@
+import { PhotoModule } from './../photo/photo.module';
+import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
-import { PhotoFormComponent } from './photo-form.component';
+
+
 import { CommonModule } from '@angular/common';
+
+import { VMessageModule } from './../../shared/components/vmessage/vmessage.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { PhotoFormComponent } from './photo-form.component';
+
 
 @NgModule({
     declarations: [PhotoFormComponent],
-    imports: [ CommonModule ]
+    imports: [
+      CommonModule,
+      FormsModule,
+      ReactiveFormsModule,
+      VMessageModule ,
+      RouterModule,
+      PhotoModule
+    ]
 })
 export class PhotoFormModule { }
